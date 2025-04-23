add68b6b1c30278394cfbbec28454bd0e3f792c3
0nalist <samrotapayne@gmail.com>
2025-04-23 16:02:04 -0400

    .

--- components/apps/ower_view/ower_view.gd
+++ components/apps/ower_view/ower_view.gd
@@ -3,9 +3,13 @@ extends BaseAppUI
 @onready var credit_label := %CreditLabel
 @onready var credit_bar := %CreditProgressBar
 @onready var credit_pay_btn := %PayCreditButton
+@onready var credit_slider := %CreditSlider
+@onready var credit_slider_label := %CreditSliderLabel
 
 @onready var loan_label := %StudentLoanLabel
 @onready var loan_pay_btn := %PayStudentLoanButton
+@onready var loan_slider := %LoanSlider
+@onready var loan_slider_label := %LoanSliderLabel
 
 @onready var credit_score_label := %CreditScoreLabel
 
@@ -15,18 +19,29 @@ func _ready():
 
 	PortfolioManager.credit_updated.connect(update_credit)
 	PortfolioManager.resource_changed.connect(_on_resource_changed)
+	PortfolioManager.cash_updated.connect(_on_cash_updated)
+
+	credit_slider.value_changed.connect(_on_credit_slider_changed)
+	loan_slider.value_changed.connect(_on_loan_slider_changed)
 
 	update_credit(PortfolioManager.credit_used, PortfolioManager.credit_limit)
 	update_student_loans()
 	update_credit_score()
+	update_sliders()
 
 func update_credit(used: float, limit: float):
 	credit_label.text = "Credit Used: $%.2f / $%.2f" % [used, limit]
 	credit_bar.value = (used / limit) * 100.0
+	update_sliders()
 
 func update_student_loans():
 	var loans := PortfolioManager.get_student_loans()
 	loan_label.text = "Student Loans: $%.2f" % loans
+	update_sliders()
+
+func update_credit_score():
+	var score = PortfolioManager.get_credit_score()
+	credit_score_label.text = "Credit Score: %d" % score
 
 func _on_resource_changed(name: String, value: float):
 	if name == "student_loans":
@@ -34,15 +49,40 @@ func _on_resource_changed(name: String, value: float):
 	elif name == "debt":
 		update_credit_score()
 
-func update_credit_score():
-	var score = PortfolioManager.get_credit_score()
-	credit_score_label.text = "Credit Score: %d" % score
+func _on_cash_updated(_cash: float):
+	update_sliders()
+
+func update_sliders():
+	var cash := PortfolioManager.cash
+
+	# Credit Slider
+	var credit_max = min(PortfolioManager.credit_used, cash)
+	credit_slider.max_value = credit_max
+	if credit_slider.value > credit_max:
+		credit_slider.value = credit_max
+	credit_slider_label.text = "$%.2f" % credit_slider.value
+
+	# Loan Slider
+	var loan_max = min(PortfolioManager.get_student_loans(), cash)
+	loan_slider.max_value = loan_max
+	if loan_slider.value > loan_max:
+		loan_slider.value = loan_max
+	loan_slider_label.text = "$%.2f" % loan_slider.value
+
+func _on_credit_slider_changed(value: float):
+	credit_slider_label.text = "$%.2f" % value
+
+func _on_loan_slider_changed(value: float):
+	loan_slider_label.text = "$%.2f" % value
 
 func _on_PayCreditButton_pressed():
-	var amount := PortfolioManager.credit_used
+	var amount = credit_slider.value
 	PortfolioManager.pay_down_credit(amount)
+	update_sliders()
 
 func _on_PayLoanButton_pressed():
-	var amount := PortfolioManager.get_student_loans()
+	var amount = loan_slider.value
 	if PortfolioManager.pay_with_cash(amount):
-		PortfolioManager.set_student_loans(0.0)
+		var new_amt = PortfolioManager.get_student_loans() - amount
+		PortfolioManager.set_student_loans(max(new_amt, 0.0))
+	update_sliders()
