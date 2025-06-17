084a5f793741d5b5874d2b3bec6cbf1923aa60f8
0nalist <163156202+0nalist@users.noreply.github.com>
2025-06-17 14:33:02 -0400

    Update npc_manager.gd

--- components/npc/npc_manager.gd
+++ components/npc/npc_manager.gd
@@ -3,21 +3,21 @@ extends Node
 
 var encounter_count: int = 0
 var encountered_npcs: Array[int] = []
-var npcs: Dictionary = {}  # idx -> NPCProfile
+var npcs: Dictionary = {}  # idx -> NPC
 var npc_overrides: Dictionary = {}  # idx -> Dictionary
 
-func encounter_new_npc(name_manager: NameManager) -> NPCProfile:
+func encounter_new_npc() -> NPC:
 	var idx = encounter_count
-	var npc = NPCFactory.create_npc(idx, name_manager)
+	var npc = NPCFactory.create_npc(idx)
 	npcs[idx] = npc
 	encountered_npcs.append(idx)
 	encounter_count += 1
 	return npc
 
-func get_npc_by_index(idx: int, name_manager: NameManager) -> NPCProfile:
+func get_npc_by_index(idx: int) -> NPC:
 	if npcs.has(idx):
 		return npcs[idx]
-	var npc = NPCFactory.create_npc(idx, name_manager)
+	var npc = NPCFactory.create_npc(idx)
 	# Only apply overrides if encountered before
 	if npc_overrides.has(idx):
 		for key in npc_overrides[idx].keys():
@@ -46,7 +46,7 @@ func to_dict() -> Dictionary:
 		"npc_data": npc_data
 	}
 
-func from_dict(data: Dictionary, name_manager: NameManager):
+func from_dict(data: Dictionary):
 	encounter_count = data.get("encounter_count", 0)
 	var npc_data = data.get("npc_data", [])
 	encountered_npcs.clear()
@@ -55,7 +55,7 @@ func from_dict(data: Dictionary, name_manager: NameManager):
 	for entry in npc_data:
 		var idx = entry.get("idx")
 		encountered_npcs.append(idx)
-		var npc = NPCFactory.create_npc(idx, name_manager)
+		var npc = NPCFactory.create_npc(idx)
 		var overrides = entry.get("overrides", {})
 		for key in overrides.keys():
 			npc.set(key, overrides[key])
