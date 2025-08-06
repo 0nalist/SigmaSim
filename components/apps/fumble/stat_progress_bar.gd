8369eae437a3908e05cd0ae2cc9d8d56ea8610f4
0nalist <samrotapayne@gmail.com>
2025-08-06 14:17:43 -0400

    .

--- components/apps/fumble/stat_progress_bar.gd
+++ components/apps/fumble/stat_progress_bar.gd
@@ -1,19 +1,21 @@
-extends ProgressBar
 class_name StatProgressBar
+extends ProgressBar
 
 @export var stat_name: String
 @export var animate: bool = true
 @export var duration: float = 0.3
 @export var easing := Tween.EASE_OUT
 @export var transition := Tween.TRANS_CUBIC
-@export var fractional:= true
+@export var fractional := true
 @export var reset_on_overflow := false
 
 var _tween: Tween = null
+var _actual_value: float = 0.0
 
 func _ready():
 	if stat_name != "":
 		PlayerManager.connect_to_stat(stat_name, self, "_on_stat_updated")
+	_actual_value = value
 	update_tooltip(value)
 
 func _exit_tree():
@@ -24,6 +26,7 @@ func _on_stat_updated(value: float) -> void:
 	update_value(value)
 
 func update_value(new_value: float) -> void:
+	_actual_value = new_value
 	var display_value = new_value
 	if reset_on_overflow:
 		display_value = fmod(new_value, max_value)
@@ -36,17 +39,19 @@ func update_value(new_value: float) -> void:
 		# Otherwise just use the float value (including > max_value if overflows)
 
 	if animate:
-		set_value_animated(display_value)
+		set_value_animated(display_value, _actual_value)
 	else:
 		value = clamp(display_value, min_value, max_value)
-	update_tooltip(display_value)
+		update_tooltip(_actual_value)
 
-func set_value_animated(target_value: float) -> void:
+func set_value_animated(target_value: float, tooltip_value: float) -> void:
 	if _tween:
 		_tween.kill()
 	_tween = get_tree().create_tween()
-	_tween.tween_property(self, "value", clamp(target_value, min_value, max_value), duration).set_trans(transition).set_ease(easing)
-	_tween.tween_callback(Callable(self, "update_tooltip").bind(target_value))
+	_tween.tween_property(
+		self, "value", clamp(target_value, min_value, max_value), duration
+	).set_trans(transition).set_ease(easing)
+	_tween.tween_callback(Callable(self, "update_tooltip").bind(tooltip_value))
 
 func update_tooltip(val: float) -> void:
 	var str_val: String
