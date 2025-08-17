a2819a7e271b7a9d28b69c0c2ccd8c2f110e67fb
0nalist <163156202+0nalist@users.noreply.github.com>
2025-08-17 14:26:31 -0400

    Fix live portrait preview updates and color picker size

--- components/portrait/portrait_creator.gd
+++ components/portrait/portrait_creator.gd
@@ -36,37 +36,38 @@ func _setup_layers() -> void:
 		var label := Label.new()
 		label.text = layer.capitalize()
 		row.add_child(label)
-		var index_btn := OptionButton.new()
-		index_btn.name = "Index"
-		var tex_arr: Array = info.get("textures", [])
-		for i in range(tex_arr.size()):
-			index_btn.add_item(str(i + 1), i)
-		index_btn.item_selected.connect(_on_index_changed.bind(layer))
-		row.add_child(index_btn)
-		var color_btn := ColorPickerButton.new()
-		color_btn.name = "Color"
-		color_btn.color_changed.connect(_on_color_changed.bind(layer))
-		row.add_child(color_btn)
-		layers_container.add_child(row)
-		layer_controls[layer] = {"index": index_btn, "color": color_btn}
+                var index_btn := OptionButton.new()
+                index_btn.name = "Index"
+                var tex_arr: Array = info.get("textures", [])
+                for i in range(tex_arr.size()):
+                        index_btn.add_item(str(i + 1), i)
+                index_btn.item_selected.connect(_on_index_changed.bind(layer))
+                row.add_child(index_btn)
+                var color_btn := ColorPickerButton.new()
+                color_btn.name = "Color"
+                color_btn.custom_minimum_size = Vector2(15, 0)
+                color_btn.color_changed.connect(_on_color_changed.bind(layer))
+                row.add_child(color_btn)
+                layers_container.add_child(row)
+                layer_controls[layer] = {"index": index_btn, "color": color_btn}
 
 
-func _on_index_changed(layer: String, idx: int) -> void:
-	config.indices[layer] = idx
-	preview.apply_config(config)
+func _on_index_changed(idx: int, layer: String) -> void:
+        config.indices[layer] = idx
+        preview.apply_config(config)
 
 
-func _on_color_changed(layer: String, color: Color) -> void:
-	if layer == "hair" or layer == "hair_back":
-		config.colors["hair"] = color
-		config.colors["hair_back"] = color
-		var other := "hair_back" if layer == "hair" else "hair"
-		var btns = layer_controls.get(other, {})
-		if btns.has("color") and btns["color"] is ColorPickerButton:
-			btns["color"].color = color
-	else:
-		config.colors[layer] = color
-	preview.apply_config(config)
+func _on_color_changed(color: Color, layer: String) -> void:
+        if layer == "hair" or layer == "hair_back":
+                config.colors["hair"] = color
+                config.colors["hair_back"] = color
+                var other := "hair_back" if layer == "hair" else "hair"
+                var btns = layer_controls.get(other, {})
+                if btns.has("color") and btns["color"] is ColorPickerButton:
+                        btns["color"].color = color
+        else:
+                config.colors[layer] = color
+        preview.apply_config(config)
 
 
 func _on_name_changed(new_text: String) -> void:
