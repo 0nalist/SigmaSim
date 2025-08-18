be709b54f8437ecbb2911d2b38892f15e579a5b2
0nalist <163156202+0nalist@users.noreply.github.com>
2025-08-18 01:03:37 -0400

    Auto-generate player portrait

--- components/ui/profile_creation/portrait_creation_screen.gd
+++ components/ui/profile_creation/portrait_creation_screen.gd
@@ -2,13 +2,19 @@ extends Control
 
 signal step_valid(valid: bool)
 
-@onready var portrait_creator: PortraitCreator = %PortraitCreator
-
 var current_config: PortraitConfig = PortraitConfig.new()
+
+@onready var portrait_creator: PortraitCreator = %PortraitCreator
 
 func _ready() -> void:
     portrait_creator.applied.connect(_on_portrait_applied)
     emit_signal("step_valid", false)
+
+    # Auto-fill the player's name and generate an initial portrait
+    var pname := PlayerManager.get_var("name", "")
+    if pname != "":
+        portrait_creator.name_edit.text = pname
+        portrait_creator._on_generate_pressed()
 
 func _on_portrait_applied(cfg: PortraitConfig) -> void:
     current_config = cfg
