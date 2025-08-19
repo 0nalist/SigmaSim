820cfdde53b608f0b9cc7bab7bcb2a55e635e991
0nalist <163156202+0nalist@users.noreply.github.com>
2025-08-19 19:33:13 -0400

    Emit upgrade signals and react to TalkTherapy

--- autoloads/events.gd
+++ autoloads/events.gd
@@ -1,8 +1,9 @@
 extends Node
 
 signal desktop_background_toggled(name: String, visible: bool)
+signal upgrade_purchased(id: String, level: int)
 
-var desktop_backgrounds := {
+var desktop_backgrounds: Dictionary = {
 	"BlueWarp": true,
 	"ComicDots1": false,
 	"ComicDots2": false,
@@ -14,3 +15,15 @@ func set_desktop_background_visible(name: String, visible: bool) -> void:
 
 func is_desktop_background_visible(name: String) -> bool:
 	return desktop_backgrounds.get(name, true)
+
+func register_upgrade_signals(ids: Array) -> void:
+	for id: String in ids:
+		var signal_name: String = "%s_purchased" % id
+		if not has_signal(signal_name):
+			add_user_signal(signal_name, [{"name": "level", "type": TYPE_INT}])
+
+func emit_upgrade_purchased(id: String, level: int) -> void:
+	upgrade_purchased.emit(id, level)
+	var signal_name: String = "%s_purchased" % id
+	if has_signal(signal_name):
+		emit_signal(signal_name, level)
