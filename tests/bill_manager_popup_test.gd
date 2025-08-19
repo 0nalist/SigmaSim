62e02ff0b89f6f26d6c2b88c929b330eb4e668bc
0nalist <163156202+0nalist@users.noreply.github.com>
2025-08-18 20:09:22 -0400

    test: add bill manager popup tests

---
+++ tests/bill_manager_popup_test.gd
@@ -0,0 +1,26 @@
+extends SceneTree
+
+func _init():
+        var bm = Engine.get_singleton("BillManager")
+        var tm = Engine.get_singleton("TimeManager")
+        var date_key = "%d/%d/%d" % [tm.current_day, tm.current_month, tm.current_year]
+        var data = {
+                "autopay_enabled": false,
+                "lifestyle_categories": {},
+                "lifestyle_indices": {},
+                "pane_data": [
+                        {
+                                "type": "BillPopupUI",
+                                "bill_name": "Rent",
+                                "amount": 1000.0,
+                                "date_key": date_key
+                        }
+                ]
+        }
+        bm.load_from_data(data)
+        assert(bm.active_bills.is_empty())
+        bm.show_due_popups()
+        assert(bm.active_bills.has(date_key))
+        assert(bm.active_bills[date_key].size() == 1)
+        print("bill_manager_popup_test passed")
+        quit()
