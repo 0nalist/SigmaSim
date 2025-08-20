25a0cbb4ecbefb64f3367e74bf5734ca8da83b0e
0nalist <163156202+0nalist@users.noreply.github.com>
2025-08-20 01:05:00 -0400

    style: fix indentation

--- components/settings_window.gd
+++ components/settings_window.gd
@@ -9,6 +9,14 @@ extends Pane
 @onready var blue_warp_button: CheckButton = %BlueWarpButton
 @onready var comic_dots1_button: CheckButton = %ComicDots1Button
 @onready var comic_dots2_button: CheckButton = %ComicDots2Button
+@onready var waves_button: CheckButton = %WavesButton
+@onready var bottom_color_picker: ColorPickerButton = %BottomColorPicker
+@onready var top_color_picker: ColorPickerButton = %TopColorPicker
+@onready var wave_amp_spin_box: SpinBox = %WaveAmpSpinBox
+@onready var wave_size_spin_box: SpinBox = %WaveSizeSpinBox
+@onready var wave_time_mul_spin_box: SpinBox = %WaveTimeMulSpinBox
+@onready var total_phases_spin_box: SpinBox = %TotalPhasesSpinBox
+@onready var waves_shader_material: ShaderMaterial = get_tree().root.get_node("Main/DesktopEnv/ShaderBackgroundsContainer/WavesShader").material
 
 
 func _ready() -> void:
@@ -25,6 +33,13 @@ func _ready() -> void:
 		blue_warp_button.button_pressed = Events.is_desktop_background_visible("BlueWarp")
 		comic_dots1_button.button_pressed = Events.is_desktop_background_visible("ComicDots1")
 		comic_dots2_button.button_pressed = Events.is_desktop_background_visible("ComicDots2")
+		waves_button.button_pressed = Events.is_desktop_background_visible("Waves")
+		bottom_color_picker.color = waves_shader_material.get_shader_parameter("bottom_color")
+		top_color_picker.color = waves_shader_material.get_shader_parameter("top_color")
+		wave_amp_spin_box.value = waves_shader_material.get_shader_parameter("wave_amp")
+		wave_size_spin_box.value = waves_shader_material.get_shader_parameter("wave_size")
+		wave_time_mul_spin_box.value = waves_shader_material.get_shader_parameter("wave_time_mul")
+		total_phases_spin_box.value = waves_shader_material.get_shader_parameter("total_phases")
 
 func update_checked_mode() -> void:
 	var mode = DisplayServer.window_get_mode()
@@ -71,7 +86,28 @@ func _on_comic_dots_1_button_toggled(toggled_on: bool) -> void:
 	Events.set_desktop_background_visible("ComicDots1", toggled_on)
 
 func _on_comic_dots_2_button_toggled(toggled_on: bool) -> void:
-		Events.set_desktop_background_visible("ComicDots2", toggled_on)
+	Events.set_desktop_background_visible("ComicDots2", toggled_on)
+
+func _on_waves_button_toggled(toggled_on: bool) -> void:
+	Events.set_desktop_background_visible("Waves", toggled_on)
+
+func _on_bottom_color_picker_color_changed(color: Color) -> void:
+	waves_shader_material.set_shader_parameter("bottom_color", color)
+
+func _on_top_color_picker_color_changed(color: Color) -> void:
+	waves_shader_material.set_shader_parameter("top_color", color)
+
+func _on_wave_amp_spin_box_value_changed(value: float) -> void:
+	waves_shader_material.set_shader_parameter("wave_amp", value)
+
+func _on_wave_size_spin_box_value_changed(value: float) -> void:
+	waves_shader_material.set_shader_parameter("wave_size", value)
+
+func _on_wave_time_mul_spin_box_value_changed(value: float) -> void:
+	waves_shader_material.set_shader_parameter("wave_time_mul", value)
+
+func _on_total_phases_spin_box_value_changed(value: float) -> void:
+	waves_shader_material.set_shader_parameter("total_phases", value)
 
 func _on_minute_passed(_total_minutes: int) -> void:
 		_update_autosave_timer_label()
