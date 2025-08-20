6e851084b164be5fc52dff7e2ffd74ffcb006399
0nalist <samrotapayne@gmail.com>
2025-08-20 17:56:27 -0400

    foldin

--- components/settings_window.gd
+++ components/settings_window.gd
@@ -50,39 +50,39 @@ func _ready() -> void:
 	if OS.has_feature("editor") or DisplayServer.get_name() == "headless":
 		fullscreen_check_box.disabled = true
 	#%SiggyButton.toggled_on = Siggy.toggled_on
-		autosave_check_box.button_pressed = TimeManager.autosave_enabled
-		_update_autosave_timer_label()
-		TimeManager.minute_passed.connect(_on_minute_passed)
-		blue_warp_button.button_pressed = Events.is_desktop_background_visible("BlueWarp")
-		comic_dots1_button.button_pressed = Events.is_desktop_background_visible("ComicDots1")
-		comic_dots2_button.button_pressed = Events.is_desktop_background_visible("ComicDots2")
-		waves_button.button_pressed = Events.is_desktop_background_visible("Waves")
-		bottom_color_picker.color = waves_shader_material.get_shader_parameter("bottom_color")
-		top_color_picker.color = waves_shader_material.get_shader_parameter("top_color")
-		wave_amp_slider.value = waves_shader_material.get_shader_parameter("wave_amp")
-		wave_size_slider.value = waves_shader_material.get_shader_parameter("wave_size")
-		wave_time_mul_slider.value = waves_shader_material.get_shader_parameter("wave_time_mul")
-		total_phases_slider.value = waves_shader_material.get_shader_parameter("total_phases")
-		blue_warp_stretch_slider.value = blue_warp_shader_material.get_shader_parameter("stretch")
-		blue_warp_thing1_slider.value = blue_warp_shader_material.get_shader_parameter("thing1")
-		blue_warp_thing2_slider.value = blue_warp_shader_material.get_shader_parameter("thing2")
-		blue_warp_thing3_slider.value = blue_warp_shader_material.get_shader_parameter("thing3")
-		blue_warp_speed_slider.value = blue_warp_shader_material.get_shader_parameter("speed")
-		comic_dots1_color_picker.color = comic_dots1_shader_material.get_shader_parameter("circle_color")
-		comic_dots1_multiplier_slider.value = comic_dots1_shader_material.get_shader_parameter("circle_multiplier")
-		comic_dots1_speed_slider.value = comic_dots1_shader_material.get_shader_parameter("speed")
-		comic_dots2_color_picker.color = comic_dots2_shader_material.get_shader_parameter("circle_color")
-		comic_dots2_multiplier_slider.value = comic_dots2_shader_material.get_shader_parameter("circle_multiplier")
-		comic_dots2_speed_slider.value = comic_dots2_shader_material.get_shader_parameter("speed")
-		electric_button.button_pressed = Events.is_desktop_background_visible("Electric")
-		electric_bg_color_picker.color = electric_shader_material.get_shader_parameter("background_color")
-		electric_line_color_picker.color = electric_shader_material.get_shader_parameter("line_color")
-		electric_freq_slider.value = electric_shader_material.get_shader_parameter("line_freq")
-		electric_height_slider.value = electric_shader_material.get_shader_parameter("height")
-		electric_speed_slider.value = electric_shader_material.get_shader_parameter("speed")
-		var electric_scale: Vector2 = electric_shader_material.get_shader_parameter("scale")
-		electric_scale_x_slider.value = electric_scale.x
-		electric_scale_y_slider.value = electric_scale.y
+	autosave_check_box.button_pressed = TimeManager.autosave_enabled
+	_update_autosave_timer_label()
+	TimeManager.minute_passed.connect(_on_minute_passed)
+	blue_warp_button.button_pressed = Events.is_desktop_background_visible("BlueWarp")
+	comic_dots1_button.button_pressed = Events.is_desktop_background_visible("ComicDots1")
+	comic_dots2_button.button_pressed = Events.is_desktop_background_visible("ComicDots2")
+	waves_button.button_pressed = Events.is_desktop_background_visible("Waves")
+	bottom_color_picker.color = waves_shader_material.get_shader_parameter("bottom_color")
+	top_color_picker.color = waves_shader_material.get_shader_parameter("top_color")
+	wave_amp_slider.value = waves_shader_material.get_shader_parameter("wave_amp")
+	wave_size_slider.value = waves_shader_material.get_shader_parameter("wave_size")
+	wave_time_mul_slider.value = waves_shader_material.get_shader_parameter("wave_time_mul")
+	total_phases_slider.value = waves_shader_material.get_shader_parameter("total_phases")
+	blue_warp_stretch_slider.value = blue_warp_shader_material.get_shader_parameter("stretch")
+	blue_warp_thing1_slider.value = blue_warp_shader_material.get_shader_parameter("thing1")
+	blue_warp_thing2_slider.value = blue_warp_shader_material.get_shader_parameter("thing2")
+	blue_warp_thing3_slider.value = blue_warp_shader_material.get_shader_parameter("thing3")
+	blue_warp_speed_slider.value = blue_warp_shader_material.get_shader_parameter("speed")
+	comic_dots1_color_picker.color = comic_dots1_shader_material.get_shader_parameter("circle_color")
+	comic_dots1_multiplier_slider.value = comic_dots1_shader_material.get_shader_parameter("circle_multiplier")
+	comic_dots1_speed_slider.value = comic_dots1_shader_material.get_shader_parameter("speed")
+	comic_dots2_color_picker.color = comic_dots2_shader_material.get_shader_parameter("circle_color")
+	comic_dots2_multiplier_slider.value = comic_dots2_shader_material.get_shader_parameter("circle_multiplier")
+	comic_dots2_speed_slider.value = comic_dots2_shader_material.get_shader_parameter("speed")
+	electric_button.button_pressed = Events.is_desktop_background_visible("Electric")
+	electric_bg_color_picker.color = electric_shader_material.get_shader_parameter("background_color")
+	electric_line_color_picker.color = electric_shader_material.get_shader_parameter("line_color")
+	electric_freq_slider.value = electric_shader_material.get_shader_parameter("line_freq")
+	electric_height_slider.value = electric_shader_material.get_shader_parameter("height")
+	electric_speed_slider.value = electric_shader_material.get_shader_parameter("speed")
+	var electric_scale: Vector2 = electric_shader_material.get_shader_parameter("scale")
+	electric_scale_x_slider.value = electric_scale.x
+	electric_scale_y_slider.value = electric_scale.y
 
 func update_checked_mode() -> void:
 	var mode = DisplayServer.window_get_mode()
@@ -135,72 +135,72 @@ func _on_waves_button_toggled(toggled_on: bool) -> void:
 	Events.set_desktop_background_visible("Waves", toggled_on)
 
 func _on_bottom_color_picker_color_changed(color: Color) -> void:
-		waves_shader_material.set_shader_parameter("bottom_color", color)
-		PlayerManager.set_shader_param("Waves", "bottom_color", color)
+	waves_shader_material.set_shader_parameter("bottom_color", color)
+	PlayerManager.set_shader_param("Waves", "bottom_color", color)
 
 func _on_top_color_picker_color_changed(color: Color) -> void:
-		waves_shader_material.set_shader_parameter("top_color", color)
-		PlayerManager.set_shader_param("Waves", "top_color", color)
+	waves_shader_material.set_shader_parameter("top_color", color)
+	PlayerManager.set_shader_param("Waves", "top_color", color)
 
 func _on_wave_amp_slider_value_changed(value: float) -> void:
-				waves_shader_material.set_shader_parameter("wave_amp", value)
-				PlayerManager.set_shader_param("Waves", "wave_amp", value)
+	waves_shader_material.set_shader_parameter("wave_amp", value)
+	PlayerManager.set_shader_param("Waves", "wave_amp", value)
 
 func _on_wave_size_slider_value_changed(value: float) -> void:
-				waves_shader_material.set_shader_parameter("wave_size", value)
-				PlayerManager.set_shader_param("Waves", "wave_size", value)
+	waves_shader_material.set_shader_parameter("wave_size", value)
+	PlayerManager.set_shader_param("Waves", "wave_size", value)
 
 func _on_wave_time_mul_slider_value_changed(value: float) -> void:
-				waves_shader_material.set_shader_parameter("wave_time_mul", value)
-				PlayerManager.set_shader_param("Waves", "wave_time_mul", value)
+	waves_shader_material.set_shader_parameter("wave_time_mul", value)
+	PlayerManager.set_shader_param("Waves", "wave_time_mul", value)
 
 func _on_total_phases_slider_value_changed(value: float) -> void:
-				waves_shader_material.set_shader_parameter("total_phases", value)
-				PlayerManager.set_shader_param("Waves", "total_phases", value)
+	waves_shader_material.set_shader_parameter("total_phases", value)
+	PlayerManager.set_shader_param("Waves", "total_phases", value)
 
 func _on_blue_warp_stretch_slider_value_changed(value: float) -> void:
-				blue_warp_shader_material.set_shader_parameter("stretch", value)
-				PlayerManager.set_shader_param("BlueWarp", "stretch", value)
+	blue_warp_shader_material.set_shader_parameter("stretch", value)
+	PlayerManager.set_shader_param("BlueWarp", "stretch", value)
 
 func _on_blue_warp_thing1_slider_value_changed(value: float) -> void:
-				blue_warp_shader_material.set_shader_parameter("thing1", value)
-				PlayerManager.set_shader_param("BlueWarp", "thing1", value)
+	blue_warp_shader_material.set_shader_parameter("thing1", value)
+	PlayerManager.set_shader_param("BlueWarp", "thing1", value)
 
 func _on_blue_warp_thing2_slider_value_changed(value: float) -> void:
-				blue_warp_shader_material.set_shader_parameter("thing2", value)
-				PlayerManager.set_shader_param("BlueWarp", "thing2", value)
+	blue_warp_shader_material.set_shader_parameter("thing2", value)
+	PlayerManager.set_shader_param("BlueWarp", "thing2", value)
 
 func _on_blue_warp_thing3_slider_value_changed(value: float) -> void:
-				blue_warp_shader_material.set_shader_parameter("thing3", value)
-				PlayerManager.set_shader_param("BlueWarp", "thing3", value)
+	blue_warp_shader_material.set_shader_parameter("thing3", value)
+	PlayerManager.set_shader_param("BlueWarp", "thing3", value)
 
 func _on_blue_warp_speed_slider_value_changed(value: float) -> void:
-				blue_warp_shader_material.set_shader_parameter("speed", value)
-				PlayerManager.set_shader_param("BlueWarp", "speed", value)
+	blue_warp_shader_material.set_shader_parameter("speed", value)
+	PlayerManager.set_shader_param("BlueWarp", "speed", value)
 
 func _on_comic_dots_1_color_picker_color_changed(color: Color) -> void:
-								comic_dots1_shader_material.set_shader_parameter("circle_color", color)
-								PlayerManager.set_shader_param("ComicDots1", "circle_color", color)
+	comic_dots1_shader_material.set_shader_parameter("circle_color", color)
+	PlayerManager.set_shader_param("ComicDots1", "circle_color", color)
 
 func _on_comic_dots_1_multiplier_slider_value_changed(value: float) -> void:
-								comic_dots1_shader_material.set_shader_parameter("circle_multiplier", value)
-								PlayerManager.set_shader_param("ComicDots1", "circle_multiplier", value)
+	comic_dots1_shader_material.set_shader_parameter("circle_multiplier", value)
+	PlayerManager.set_shader_param("ComicDots1", "circle_multiplier", value)
 
 func _on_comic_dots_1_speed_slider_value_changed(value: float) -> void:
-								comic_dots1_shader_material.set_shader_parameter("speed", value)
-								PlayerManager.set_shader_param("ComicDots1", "speed", value)
+	comic_dots1_shader_material.set_shader_parameter("speed", value)
+	PlayerManager.set_shader_param("ComicDots1", "speed", value)
 
 func _on_comic_dots_2_color_picker_color_changed(color: Color) -> void:
-								comic_dots2_shader_material.set_shader_parameter("circle_color", color)
-								PlayerManager.set_shader_param("ComicDots2", "circle_color", color)
+	comic_dots2_shader_material.set_shader_parameter("circle_color", color)
+	PlayerManager.set_shader_param("ComicDots2", "circle_color", color)
 
 func _on_comic_dots_2_multiplier_slider_value_changed(value: float) -> void:
-								comic_dots2_shader_material.set_shader_parameter("circle_multiplier", value)
-								PlayerManager.set_shader_param("ComicDots2", "circle_multiplier", value)
+	comic_dots2_shader_material.set_shader_parameter("circle_multiplier", value)
+	PlayerManager.set_shader_param("ComicDots2", "circle_multiplier", value)
 
 func _on_comic_dots_2_speed_slider_value_changed(value: float) -> void:
-								comic_dots2_shader_material.set_shader_parameter("speed", value)
-								PlayerManager.set_shader_param("ComicDots2", "speed", value)
+	comic_dots2_shader_material.set_shader_parameter("speed", value)
+	PlayerManager.set_shader_param("ComicDots2", "speed", value)
 
 func _on_electric_button_toggled(toggled_on: bool) -> void:
 		Events.set_desktop_background_visible("Electric", toggled_on)
