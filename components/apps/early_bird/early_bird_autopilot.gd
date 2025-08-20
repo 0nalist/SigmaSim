00b51b2aca6cdf99643989334d5207162d17a92a
0nalist <samrotapayne@gmail.com>
2025-08-20 02:44:58 -0400

    .

--- components/apps/early_bird/early_bird_autopilot.gd
+++ components/apps/early_bird/early_bird_autopilot.gd
@@ -15,23 +15,22 @@ func _process(delta: float) -> void:
 
 	var pipes = pipe_manager.get_active_pipe_pairs()
 	if pipes.is_empty():
-		if player.global_position.y > 300:
+		if player.position.y > 300:
 			player.flap()
 		return
 
 	# Find the first pipe ahead of the player
 	var next_pipe = null
 	for pipe in pipes:
-		if pipe.global_position.x > player.global_position.x - 40: # Adjust magic number for fine tuning
+		if pipe.position.x > player.position.x - 40: # Adjust magic number for fine tuning
 			next_pipe = pipe
 			break
-	
-	if player.global_position.y > 550:
-			player.flap()
+
+	if player.position.y > 550:
+		player.flap()
 	
 	if next_pipe:
-		var target_y = next_pipe.get_gap_center_y() #- 51 # Adjust magic number for fine tuning
-		if player.global_position.y > target_y:
-			player.flap()
-			
-	#print(player.global_position)
+			var target_y = next_pipe.get_gap_center_y() #- 51 # Adjust magic number for fine tuning
+			if player.position.y > target_y:
+					player.flap()
+		#print(player.position)
