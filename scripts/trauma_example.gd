05f224aa27471107e2b3da8b19f5f0f82d4d807d
0nalist <163156202+0nalist@users.noreply.github.com>
2025-08-21 02:30:41 -0400

    feat: add reusable trauma system

---
+++ scripts/trauma_example.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var target_control: Control
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		var ev: InputEventKey = event as InputEventKey
+		if ev.keycode == KEY_SPACE:
+			TraumaManager.hit_global(0.6)
+		if ev.keycode == KEY_S and target_control != null:
+			TraumaManager.hit_pane(target_control, 0.6)
