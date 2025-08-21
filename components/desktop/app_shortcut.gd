aae36a81b9b17b6240e4292d429659b6dcf61606
0nalist <163156202+0nalist@users.noreply.github.com>
2025-08-21 13:50:02 -0400

    Revert "Enable dragging shortcuts between folders and desktop"

--- components/desktop/app_shortcut.gd
+++ components/desktop/app_shortcut.gd
@@ -11,7 +11,6 @@ class_name AppShortcut
 
 var is_dragging: bool = false
 var drag_offset: Vector2
-var original_parent_id: int = 0
 
 func _ready() -> void:
 	icon_rect.texture = icon
@@ -27,31 +26,10 @@ func _on_gui_input(event: InputEvent) -> void:
 			elif mb.pressed:
 				is_dragging = true
 				drag_offset = mb.position
-				var data: Dictionary = DesktopLayoutManager.get_item(item_id)
-				original_parent_id = int(data.get("parent_id", 0))
-				if get_parent() is GridContainer:
-					var desktop: Node = get_tree().root.get_node_or_null("Main/DesktopEnv")
-					if desktop != null:
-						get_parent().remove_child(self)
-						desktop.add_child(self)
-						global_position = get_global_mouse_position() - drag_offset
 			else:
 				if is_dragging:
 					is_dragging = false
-					var target: Control = get_viewport().gui_pick(get_viewport().get_mouse_position())
-					var new_parent: int = 0
-					while target != null:
-						if target is FolderShortcut:
-							new_parent = target.item_id
-							break
-						if target is FolderWindow:
-							new_parent = target.folder_id
-							break
-						target = target.get_parent()
-					DesktopLayoutManager.move_item(item_id, global_position, new_parent)
-					if new_parent != 0:
-						queue_free()
+					DesktopLayoutManager.move_item(item_id, global_position)
 	elif event is InputEventMouseMotion:
 		if is_dragging:
 			global_position = get_global_mouse_position() - drag_offset
-
