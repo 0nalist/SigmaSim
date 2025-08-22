a07db07d16eaafbdf76b62bae82bf1a5da75d208
0nalist <163156202+0nalist@users.noreply.github.com>
2025-08-22 17:08:05 -0400

    Create AffinityBar custom progress bar

---
+++ components/popups/affinity_bar.gd
@@ -0,0 +1,20 @@
+class_name AffinityBar
+extends StatProgressBar
+
+var affinity_equilibrium: float = 0.0
+
+func set_affinity_equilibrium(new_eq: float) -> void:
+	affinity_equilibrium = new_eq
+	queue_redraw()
+
+func _draw() -> void:
+	var bar_rect: Rect2 = Rect2(Vector2.ZERO, size)
+	if max_value != min_value:
+		var fraction: float = clamp((affinity_equilibrium - min_value) / (max_value - min_value), 0.0, 1.0)
+		var x: float = bar_rect.position.x + bar_rect.size.x * fraction
+		draw_line(
+			Vector2(x, bar_rect.position.y),
+			Vector2(x, bar_rect.position.y + bar_rect.size.y),
+			Color.WHITE,
+			1.0
+		)
