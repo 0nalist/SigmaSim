63e7a5f520bcbc21b4f7edb4bb20853d7ccf55ff
0nalist <163156202+0nalist@users.noreply.github.com>
2025-08-26 16:35:31 -0400

    Fix ExFactor random first name tag

--- tests/randomfirstname_parse_test.gd
+++ tests/randomfirstname_parse_test.gd
@@ -1,9 +1,14 @@
 extends SceneTree
 
 func _ready() -> void:
-    var parsed := MarkupParser.parse("Hello {randomfirstname}", null)
-    assert(parsed.find("{randomfirstname}") == -1)
-    assert(parsed != "Hello FirstName")
-    assert(parsed != "Hello ?")
-    print("randomfirstname_parse_test passed")
+    var parsed1 := MarkupParser.parse("Hello {randomfirstname}", null)
+    assert(parsed1.find("{randomfirstname}") == -1)
+    assert(parsed1 != "Hello FirstName")
+    assert(parsed1 != "Hello ?")
+
+    var parsed2 := MarkupParser.parse("Hello {random_first_name}", null)
+    assert(parsed2.find("{random_first_name}") == -1)
+    assert(parsed2 != "Hello FirstName")
+    assert(parsed2 != "Hello ?")
+    print("random firstname variants parse test passed")
     quit()
