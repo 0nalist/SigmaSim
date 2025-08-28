a9412e8d6c750f4637925846a97cb339c6ac99bb
0nalist <samrotapayne@gmail.com>
2025-08-28 01:32:36 -0400

    .

--- components/apps/broke_rage/stock_row.gd
+++ components/apps/broke_rage/stock_row.gd
@@ -76,7 +76,7 @@ func update_sentiment_arrow(sentiment: float) -> void:
 
 
 func _on_stock_label_gui_input(event: InputEvent) -> void:
-       if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-               var popup_scene = preload("res://components/popups/stock_popup_ui.tscn")
-               var stock_popup = popup_scene.instantiate() as Pane
-               WindowManager.launch_pane_instance(stock_popup, stock)
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			var popup_scene = preload("res://components/popups/stock_popup_ui.tscn")
+			var stock_popup = popup_scene.instantiate() as Pane
+			WindowManager.launch_pane_instance(stock_popup, stock)
