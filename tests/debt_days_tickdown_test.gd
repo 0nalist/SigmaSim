cd3372931e3526b77b22a15190cc005b5cf66af5
0nalist <163156202+0nalist@users.noreply.github.com>
2025-08-28 01:05:15 -0400

    Add hour and minute debt countdown tests

--- tests/debt_days_tickdown_test.gd
+++ tests/debt_days_tickdown_test.gd
@@ -1,18 +1,18 @@
 extends SceneTree
 
 func _ready():
-	var bm = Engine.get_singleton("BillManager")
-	var tm = Engine.get_singleton("TimeManager")
-	bm.reset()
-	tm.reset()
-	bm.add_debt_resource({
-		"name": "Test Debt",
-		"balance": 0.0,
-		"days_until_due": 5
-	})
-	tm._advance_time(1440)
-	var res = bm.get_debt_resources()[0]
-	assert(res.get("days_until_due") == 4)
-	print("debt_days_tickdown_test passed")
-	quit()
+    var bm = Engine.get_singleton("BillManager")
+    var tm = Engine.get_singleton("TimeManager")
+    bm.reset()
+    tm.reset()
+    bm.add_debt_resource({
+        "name": "Test Debt",
+        "balance": 0.0,
+        "days_until_due": 5
+    })
+    tm._advance_time(1440)
+    var res = bm.get_debt_resources()[0]
+    assert(res.get("days_until_due") == 4)
+    print("debt_days_tickdown_test passed")
+    quit()
 
