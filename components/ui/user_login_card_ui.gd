c3b20456cb5b5bc94007bae655bfee48115c4261
0nalist <163156202+0nalist@users.noreply.github.com>
2025-08-27 23:57:32 -0400

    style: normalize tabs for login card

--- components/ui/user_login_card_ui.gd
+++ components/ui/user_login_card_ui.gd
@@ -23,119 +23,119 @@ var tween: Tween
 var expanded := false
 
 func _ready() -> void:
-        custom_minimum_size = Vector2(180, collapsed_height)
-        log_in_button.hide()
-        net_worth_label.hide()
-        password_hbox.hide()
-        show_password_button.toggled.connect(_on_show_password_button_toggled)
-        if pending_data:
-                _apply_profile_data()
+	custom_minimum_size = Vector2(180, collapsed_height)
+	log_in_button.hide()
+	net_worth_label.hide()
+	password_hbox.hide()
+	show_password_button.toggled.connect(_on_show_password_button_toggled)
+	if pending_data:
+		_apply_profile_data()
 
 func _gui_input(event: InputEvent) -> void:
-        if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-                emit_signal("card_selected", self)
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		emit_signal("card_selected", self)
 
 func set_profile_data(data: Dictionary, id: int) -> void:
-        pending_data = data
-        pending_slot_id = id
-        if is_inside_tree():
-                _apply_profile_data()
+	pending_data = data
+	pending_slot_id = id
+	if is_inside_tree():
+		_apply_profile_data()
 
 func _apply_profile_data() -> void:
-        name_label.text = pending_data.get("name", "Unnamed")
-        username_label.text = "@%s" % pending_data.get("username", "user")
+	name_label.text = pending_data.get("name", "Unnamed")
+	username_label.text = "@%s" % pending_data.get("username", "user")
 
-        var net := pending_data.get("net_worth", 0.0)
-        if Engine.has_singleton("NumberFormatter"):
-                net_worth_label.text = "$" + NumberFormatter.format_commas(net, 2, true)
-        else:
-                net_worth_label.text = "$%s" % str(net)
+	var net := pending_data.get("net_worth", 0.0)
+	if Engine.has_singleton("NumberFormatter"):
+		net_worth_label.text = "$" + NumberFormatter.format_commas(net, 2, true)
+	else:
+		net_worth_label.text = "$%s" % str(net)
 
-        var using_random_seed = pending_data.get("using_random_seed", false)
-        if using_random_seed:
-                password_line_edit.text = ""
-                password_line_edit.hide()
-                show_password_button.button_pressed = false
-                show_password_button.text = "show"
-                show_password_button.hide()
-                rainbow_password_label.bbcode_text = _generate_rainbow_password()
-                rainbow_password_label.show()
-        else:
-                password_line_edit.show()
-                show_password_button.show()
-                rainbow_password_label.hide()
-                password_line_edit.text = pending_data.get("password", "")
-                password_line_edit.secret = true
-                show_password_button.button_pressed = false
-                show_password_button.text = "show"
+	var using_random_seed = pending_data.get("using_random_seed", false)
+	if using_random_seed:
+		password_line_edit.text = ""
+		password_line_edit.hide()
+		show_password_button.button_pressed = false
+		show_password_button.text = "show"
+		show_password_button.hide()
+		rainbow_password_label.bbcode_text = _generate_rainbow_password()
+		rainbow_password_label.show()
+	else:
+		password_line_edit.show()
+		show_password_button.show()
+		rainbow_password_label.hide()
+		password_line_edit.text = pending_data.get("password", "")
+		password_line_edit.secret = true
+		show_password_button.button_pressed = false
+		show_password_button.text = "show"
 
-        var cfg_dict = pending_data.get("portrait_config", {})
-        if cfg_dict is Dictionary:
-                var cfg = PortraitConfig.from_dict(cfg_dict)
-                profile_pic.apply_config(cfg)
-                profile_pic.custom_minimum_size = Vector2(128, 128)
-                profile_pic.set_size(Vector2(128, 128))
-                profile_pic.size_flags_horizontal = Control.SIZE_FILL
-                profile_pic.size_flags_vertical = Control.SIZE_FILL
+	var cfg_dict = pending_data.get("portrait_config", {})
+	if cfg_dict is Dictionary:
+		var cfg = PortraitConfig.from_dict(cfg_dict)
+		profile_pic.apply_config(cfg)
+		profile_pic.custom_minimum_size = Vector2(128, 128)
+		profile_pic.set_size(Vector2(128, 128))
+		profile_pic.size_flags_horizontal = Control.SIZE_FILL
+		profile_pic.size_flags_vertical = Control.SIZE_FILL
 
-        slot_id = pending_slot_id
+	slot_id = pending_slot_id
 
 func expand() -> void:
-        if expanded:
-                return
-        expanded = true
-        net_worth_label.show()
-        password_hbox.show()
-        log_in_button.show()
-        if tween:
-                tween.kill()
-        tween = create_tween()
-        tween.tween_property(self, "custom_minimum_size:y", expanded_height, 0.25)
-        tween.parallel().tween_property(net_worth_label, "modulate:a", 1.0, 0.25).from(0.0)
-        tween.parallel().tween_property(password_hbox, "modulate:a", 1.0, 0.25).from(0.0)
+	if expanded:
+		return
+	expanded = true
+	net_worth_label.show()
+	password_hbox.show()
+	log_in_button.show()
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(self, "custom_minimum_size:y", expanded_height, 0.25)
+	tween.parallel().tween_property(net_worth_label, "modulate:a", 1.0, 0.25).from(0.0)
+	tween.parallel().tween_property(password_hbox, "modulate:a", 1.0, 0.25).from(0.0)
 
 func collapse() -> void:
-        if not expanded:
-                return
-        expanded = false
-        if tween:
-                tween.kill()
-        tween = create_tween()
-        tween.tween_property(self, "custom_minimum_size:y", collapsed_height, 0.25)
-        tween.parallel().tween_property(net_worth_label, "modulate:a", 0.0, 0.25)
-        tween.parallel().tween_property(password_hbox, "modulate:a", 0.0, 0.25)
-        tween.finished.connect(func():
-                net_worth_label.hide()
-                password_hbox.hide()
-                log_in_button.hide()
-        )
+	if not expanded:
+		return
+	expanded = false
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(self, "custom_minimum_size:y", collapsed_height, 0.25)
+	tween.parallel().tween_property(net_worth_label, "modulate:a", 0.0, 0.25)
+	tween.parallel().tween_property(password_hbox, "modulate:a", 0.0, 0.25)
+	tween.finished.connect(func():
+		net_worth_label.hide()
+		password_hbox.hide()
+		log_in_button.hide()
+	)
 
 func _on_log_in_button_pressed() -> void:
-        emit_signal("login_requested", slot_id)
+	emit_signal("login_requested", slot_id)
 
 func _on_show_password_button_toggled(toggled_on: bool) -> void:
-        password_line_edit.secret = not toggled_on
-        if toggled_on:
-                show_password_button.text = "hide"
-        else:
-                show_password_button.text = "show"
+	password_line_edit.secret = not toggled_on
+	if toggled_on:
+		show_password_button.text = "hide"
+	else:
+		show_password_button.text = "show"
 
 func _generate_rainbow_password() -> String:
-        var text := "password"
-        var colors := [
-                Color(1, 0, 0),
-                Color(1, 0.5, 0),
-                Color(1, 1, 0),
-                Color(0, 1, 0),
-                Color(0, 0, 1),
-                Color(0.29, 0, 0.51),
-                Color(0.56, 0, 1),
-        ]
-        var rng := RandomNumberGenerator.new()
-        rng.randomize()
-        var bbcode := ""
-        for c in text:
-                var color = colors[rng.randi_range(0, colors.size() - 1)]
-                bbcode += "[color=%s]%s[/color]" % [color.to_html(), c]
-        return bbcode
+	var text := "password"
+	var colors := [
+		Color(1, 0, 0),
+		Color(1, 0.5, 0),
+		Color(1, 1, 0),
+		Color(0, 1, 0),
+		Color(0, 0, 1),
+		Color(0.29, 0, 0.51),
+		Color(0.56, 0, 1),
+	]
+	var rng := RandomNumberGenerator.new()
+	rng.randomize()
+	var bbcode := ""
+	for c in text:
+		var color = colors[rng.randi_range(0, colors.size() - 1)]
+		bbcode += "[color=%s]%s[/color]" % [color.to_html(), c]
+	return bbcode
 
