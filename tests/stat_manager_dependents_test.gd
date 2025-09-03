7d3ffa54cb9a1b10b226ec0fceb3d1481fb50cee
0nalist <samrotapayne@gmail.com>
2025-09-03 01:25:15 -0400

    .

--- tests/stat_manager_dependents_test.gd
+++ tests/stat_manager_dependents_test.gd
@@ -1,11 +1,11 @@
 extends SceneTree
 
 func _ready():
-    var sm = Engine.get_singleton("StatManager")
-    sm._build_dependents_map()
-    for dep in sm._dependents.keys():
-        var arr = sm._dependents[dep]
-        for item in arr:
-            assert(arr.count(item) == 1, "%s appears multiple times in dependents for %s" % [item, dep])
-    print("stat_manager_dependents_test passed")
-    quit()
+	var sm = Engine.get_singleton("StatManager")
+	sm._build_dependents_map()
+	for dep in sm._dependents.keys():
+		var arr = sm._dependents[dep]
+		for item in arr:
+			assert(arr.count(item) == 1, "%s appears multiple times in dependents for %s" % [item, dep])
+	print("stat_manager_dependents_test passed")
+	quit()
