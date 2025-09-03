6ad64e39e2bb80544609dc168d365a12eb34d305
0nalist <samrotapayne@gmail.com>
2025-09-03 10:22:11 -0400

    .

--- tests/autoworm_cost_test.gd
+++ tests/autoworm_cost_test.gd
@@ -1,17 +1,17 @@
 extends SceneTree
 
 func _ready() -> void:
-    var id := "earlybird_autoworm"
-    var prev_level := UpgradeManager.get_level(id)
-    UpgradeManager.player_levels[id] = 0
-    var cost1 := UpgradeManager.get_cost_for_next_level(id).get("ex", -1)
-    UpgradeManager.player_levels[id] = 1
-    var cost2 := UpgradeManager.get_cost_for_next_level(id).get("ex", -1)
-    UpgradeManager.player_levels[id] = 2
-    var cost3 := UpgradeManager.get_cost_for_next_level(id).get("ex", -1)
-    assert(cost1 == 100)
-    assert(cost2 == 200)
-    assert(cost3 == 400)
-    UpgradeManager.player_levels[id] = prev_level
-    print("autoworm_cost_test passed")
-    quit()
+	var id := "earlybird_autoworm"
+	var prev_level := UpgradeManager.get_level(id)
+	UpgradeManager.player_levels[id] = 0
+	var cost1 = UpgradeManager.get_cost_for_next_level(id).get("ex", -1)
+	UpgradeManager.player_levels[id] = 1
+	var cost2 = UpgradeManager.get_cost_for_next_level(id).get("ex", -1)
+	UpgradeManager.player_levels[id] = 2
+	var cost3 = UpgradeManager.get_cost_for_next_level(id).get("ex", -1)
+	assert(cost1 == 100)
+	assert(cost2 == 200)
+	assert(cost3 == 400)
+	UpgradeManager.player_levels[id] = prev_level
+	print("autoworm_cost_test passed")
+	quit()
--- tests/autoworm_upgrade_test.gd
+++ tests/autoworm_upgrade_test.gd
@@ -1,26 +1,25 @@
-extends SceneTree
+extends Node
 
 func _ready() -> void:
-    var id := "earlybird_autoworm"
-    var prev_level := UpgradeManager.get_level(id)
-    var prev_cash := PortfolioManager.cash
-    UpgradeManager.player_levels[id] = 10
-    PortfolioManager.cash = 0
-    StatManager.recalculate_all_stats_once()
+	var id := "earlybird_autoworm"
+	var prev_level := UpgradeManager.get_level(id)
+	var prev_cash := PortfolioManager.cash
+	UpgradeManager.player_levels[id] = 10
+	PortfolioManager.cash = 0
+	StatManager.recalculate_all_stats_once()
 
-    var worm := Area2D.new()
-    worm.set_script(load("res://components/apps/early_bird/worm.gd"))
-    var sprite := Sprite2D.new(); sprite.name = "Sprite2D"; worm.add_child(sprite)
-    var tex := TextureRect.new(); tex.name = "WormTexture"; worm.add_child(tex)
-    var timer := Timer.new(); timer.name = "Timer"; timer.autostart = true; worm.add_child(timer)
-    add_child(worm)
-    await get_tree().process_frame
-    worm.show()
+	var worm := Area2D.new()
+	worm.set_script(load("res://components/apps/early_bird/worm.gd"))
+	var sprite := Sprite2D.new(); sprite.name = "Sprite2D"; worm.add_child(sprite)
+	var tex := TextureRect.new(); tex.name = "WormTexture"; worm.add_child(tex)
+	var timer := Timer.new(); timer.name = "Timer"; timer.autostart = true; worm.add_child(timer)
+	add_child(worm)
+	await get_tree().process_frame
+	worm.show()
 
-    await get_tree().create_timer(1.2).timeout
-    assert(PortfolioManager.cash >= 1.0)
+	await get_tree().create_timer(1.2).timeout
+	assert(PortfolioManager.cash >= 1.0)
 
-    UpgradeManager.player_levels[id] = prev_level
-    PortfolioManager.cash = prev_cash
-    print("autoworm_upgrade_test passed")
-    quit()
+	UpgradeManager.player_levels[id] = prev_level
+	PortfolioManager.cash = prev_cash
+	print("autoworm_upgrade_test passed")
