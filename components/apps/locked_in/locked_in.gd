ac02261329c6d887a804d67830ffb8e4fcf816a5
0nalist <163156202+0nalist@users.noreply.github.com>
2025-09-04 15:22:28 -0400

    Integrate locked in connections list

--- components/apps/locked_in/locked_in.gd
+++ components/apps/locked_in/locked_in.gd
@@ -1,1 +1,11 @@
 extends Pane
+
+@onready var connections_list: LockedInConnectionsListUI = %ConnectionsList
+@onready var profile_ui: LockedInProfileUI = %Profile
+
+func _ready() -> void:
+	var ids: PackedInt32Array = NPCManager.get_locked_in_connection_ids()
+	connections_list.populate(ids)
+	connections_list.connection_selected.connect(func(npc_id: int) -> void:
+		profile_ui.load_npc(NPCManager.get_npc_by_index(npc_id))
+	)
