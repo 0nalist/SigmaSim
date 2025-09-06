2adb142bbf64a6b4b02941e54a7f62d96521359d
0nalist <163156202+0nalist@users.noreply.github.com>
2025-09-06 10:30:38 -0400

    Restore upside-down tarot card orientation

--- components/apps/tarot/tarot_app.gd
+++ components/apps/tarot/tarot_app.gd
@@ -94,6 +94,7 @@ func _show_last_drawn_card() -> void:
 	var t = create_tween()
 	t.tween_property(view, "modulate:a", 1.0, 0.3)
 	if upside_down:
+			view.texture_rect.rotation_degrees = 0
 			t.tween_property(view.texture_rect, "rotation_degrees", 180, 0.3)
 
 func _show_reading_cards(cards: Array) -> void:
@@ -115,6 +116,7 @@ func _show_reading_cards(cards: Array) -> void:
 		var t = create_tween()
 		t.tween_property(view, "modulate:a", 1.0, 0.3).set_delay(index * 0.2)
 		if upside_down:
+			view.texture_rect.rotation_degrees = 0
 			t.tween_property(view.texture_rect, "rotation_degrees", 180, 0.3)
 		index += 1
 
