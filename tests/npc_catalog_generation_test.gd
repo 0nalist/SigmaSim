6930d11783012001ce636bef7954168072180d7c
0nalist <samrotapayne@gmail.com>
2025-09-08 00:07:06 -0400

    .

--- tests/npc_catalog_generation_test.gd
+++ tests/npc_catalog_generation_test.gd
@@ -2,12 +2,12 @@ extends SceneTree
 
 func _ready() -> void:
 	NPCCatalog.generate(100)
-	var total := min(100, NameManager.get_unique_name_count())
+	var total = min(100, NameManager.get_unique_name_count())
 	assert(NPCCatalog.npc_catalog.size() == total)
 	var arr := NPCCatalog.index_by_attractiveness
 	for i in range(1, arr.size()):
 		assert(NPCCatalog.npc_catalog[arr[i-1]]["attractiveness"] <= NPCCatalog.npc_catalog[arr[i]]["attractiveness"])
-	var fem := NPCCatalog.index_by_gender["fem"]
+	var fem = NPCCatalog.index_by_gender["fem"]
 	for i in range(1, fem.size()):
 		assert(NPCCatalog.npc_catalog[fem[i-1]]["gender_vector"].x <= NPCCatalog.npc_catalog[fem[i]]["gender_vector"].x)
 	print("npc_catalog_generation_test passed")
