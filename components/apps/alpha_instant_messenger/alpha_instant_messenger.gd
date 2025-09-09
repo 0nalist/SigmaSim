20a93427e0e48b2bc058399e956c0386bc8ca1bd
0nalist <samrotapayne@gmail.com>
2025-09-09 18:01:04 -0400

    Revert "Merge pull request #740 from 0nalist/codex/create-aim_contact_button-scene"
    This reverts commit d2c0e64a065bb5195be9e00b3c9c7cf30603cf53, reversing
    changes made to 2321430b324edde3104c2acee80420578b3241b3.

--- components/apps/alpha_instant_messenger/alpha_instant_messenger.gd
+++ components/apps/alpha_instant_messenger/alpha_instant_messenger.gd
@@ -2,7 +2,6 @@ extends Pane
 class_name AlphaInstantMessenger
 
 const AIM_CHAT_UI_SCENE: PackedScene = preload("res://components/apps/alpha_instant_messenger/aim_chat_ui.tscn")
-const AIM_CONTACT_BUTTON_SCENE: PackedScene = preload("res://components/apps/alpha_instant_messenger/aim_contact_button.tscn")
 
 @onready var contacts_vbox: VBoxContainer = %ContactsVBox
 
@@ -14,16 +13,15 @@ func _populate_contacts() -> void:
 	for child in contacts_vbox.get_children():
 
 		child.queue_free()
-        var entries: Array[int] = NPCManager.get_romantic_npcs()
-        for idx in entries:
-                var npc: NPC = NPCManager.get_npc_by_index(int(idx))
-                var btn: AimContactButton = AIM_CONTACT_BUTTON_SCENE.instantiate()
-                btn.text = "@%s" % npc.username
-                btn.set_npc(npc)
-                btn.pressed.connect(func() -> void:
-                        _open_chat_ui(int(idx), npc)
-                )
-                contacts_vbox.add_child(btn)
+	var entries: Array[int] = NPCManager.get_romantic_npcs()
+	for idx in entries:
+		var npc: NPC = NPCManager.get_npc_by_index(int(idx))
+		var btn: AimContactButton = AimContactButton.new()
+		btn.text = "@%s" % npc.username
+		btn.pressed.connect(func() -> void:
+			_open_chat_ui(int(idx), npc)
+		)
+		contacts_vbox.add_child(btn)
 
 
 func _open_chat_ui(idx: int, npc: NPC) -> void:
