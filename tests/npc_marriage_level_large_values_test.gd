e5236cb6820ab2094ec92d9eb0f6b5a3fd2e72c0
0nalist <163156202+0nalist@users.noreply.github.com>
2025-09-09 17:14:47 -0400

    Handle marriage level with base-10 log

---
+++ tests/npc_marriage_level_large_values_test.gd
@@ -0,0 +1,14 @@
+extends SceneTree
+
+func _ready() -> void:
+        load("res://flex_number.gd")
+        load("res://resources/context_action.gd")
+        load("res://resources/portraits/portrait_config.gd")
+        var npc_script = load("res://components/npc/npc.gd")
+        var npc = npc_script.new()
+        npc.relationship_progress.set_value(1e20)
+        assert(npc.get_marriage_level() == 16)
+        npc.relationship_progress.set_value(1e40)
+        assert(npc.get_marriage_level() == 36)
+        print("npc_marriage_level_large_values_test passed")
+        quit()
