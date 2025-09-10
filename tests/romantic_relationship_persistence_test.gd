35e0c1f1a7bd061e7b246965f7b7afd3fc0c898a
0nalist <samrotapayne@gmail.com>
2025-09-10 15:16:40 -0400

    .

--- tests/romantic_relationship_persistence_test.gd
+++ tests/romantic_relationship_persistence_test.gd
@@ -1,20 +1,20 @@
 extends SceneTree
 
 func _ready() -> void:
-    var save_mgr = Engine.get_singleton("SaveManager")
-    var npc_mgr = Engine.get_singleton("NPCManager")
+	var save_mgr = Engine.get_singleton("SaveManager")
+	var npc_mgr = Engine.get_singleton("NPCManager")
 
-    save_mgr.reset_managers()
-    save_mgr.current_slot_id = 1
+	save_mgr.reset_managers()
+	save_mgr.current_slot_id = 1
 
-    var npc_id := 4242
-    npc_mgr.get_npc_by_index(npc_id)
-    npc_mgr.add_romantic_npc(npc_id)
+	var npc_id := 4242
+	npc_mgr.get_npc_by_index(npc_id)
+	npc_mgr.add_romantic_npc(npc_id)
 
-    save_mgr.save_to_slot(save_mgr.current_slot_id)
-    save_mgr.reset_managers()
-    save_mgr.load_from_slot(save_mgr.current_slot_id)
+	save_mgr.save_to_slot(save_mgr.current_slot_id)
+	save_mgr.reset_managers()
+	save_mgr.load_from_slot(save_mgr.current_slot_id)
 
-    assert(npc_mgr.get_romantic_npcs().has(npc_id))
-    print("romantic_relationship_persistence_test passed")
-    quit()
+	assert(npc_mgr.get_romantic_npcs().has(npc_id))
+	print("romantic_relationship_persistence_test passed")
+	quit()
