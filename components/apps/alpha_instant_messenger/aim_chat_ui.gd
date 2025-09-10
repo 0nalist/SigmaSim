1c257d66632d5528e232a52b5f69e883f1c1fff5
0nalist <samrotapayne@gmail.com>
2025-09-10 16:21:14 -0400

    chatlogrefac

--- components/apps/alpha_instant_messenger/aim_chat_ui.gd
+++ components/apps/alpha_instant_messenger/aim_chat_ui.gd
@@ -30,8 +30,8 @@ func _ready() -> void:
 	greet_button.pressed.connect(_on_greet_pressed)
 	gift_button.pressed.connect(_on_gift_pressed)
 	date_button.pressed.connect(_on_date_pressed)
-        relationship_button.pressed.connect(_on_relationship_pressed)
-        line_edit.text_submitted.connect(_on_line_edit_submitted)
+	relationship_button.pressed.connect(_on_relationship_pressed)
+	line_edit.text_submitted.connect(_on_line_edit_submitted)
 
 func _finalize_setup() -> void:
 	if npc == null:
@@ -71,13 +71,13 @@ func _on_date_pressed() -> void:
 	pass
 
 func _on_relationship_pressed() -> void:
-        pass
+		pass
 
 func _on_line_edit_submitted(new_text: String) -> void:
-        var text = new_text.strip_edges()
-        if text == "":
-                return
-        var log: AimChatLog = AIM_CHAT_LOG_SCENE.instantiate()
-        messages_container.add_child(log)
-        log.set_message(text, true)
-        line_edit.clear()
+	var text = new_text.strip_edges()
+	if text == "":
+			return
+	var log: AimChatLog = AIM_CHAT_LOG_SCENE.instantiate()
+	messages_container.add_child(log)
+	log.set_message(text, true)
+	line_edit.clear()
