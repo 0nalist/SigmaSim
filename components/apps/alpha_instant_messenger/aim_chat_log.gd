06fe0434c36e8ae83355f12de279786c7f7df6a2
0nalist <163156202+0nalist@users.noreply.github.com>
2025-09-10 16:38:55 -0400

    Refactor AIM Chat components to PanelContainers

--- components/apps/alpha_instant_messenger/aim_chat_log.gd
+++ components/apps/alpha_instant_messenger/aim_chat_log.gd
@@ -1,9 +1,11 @@
-extends VBoxContainer
+extends PanelContainer
 class_name AimChatLog
 
 const AIM_CHAT_MESSAGE_SCENE: PackedScene = preload("res://components/apps/alpha_instant_messenger/aim_chat_message.tscn")
 
+@onready var messages_container: VBoxContainer = $Messages
+
 func add_message(text: String, from_player: bool) -> void:
     var message: AimChatMessage = AIM_CHAT_MESSAGE_SCENE.instantiate()
-    add_child(message)
+    messages_container.add_child(message)
     message.set_message(text, from_player)
